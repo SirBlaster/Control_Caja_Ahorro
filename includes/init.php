9e4a693ef943de30e5dbbf824ac6d95f70b34fc9
DiegoHR04 <huerta.rodriguez.diego.cbtis165@gmail.com>
2025-12-08 19:47:35 -0600

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -18,6 +18,8 @@ require_once __DIR__ . '/security.php';
 // Incluir funciones de usuario
 require_once __DIR__ . '/user_functions.php';
 
+require_once 'admin_user_handlers.php';
+
 // Configuraciones
 date_default_timezone_set('America/Mexico_City');
 
