0735fd95086ef5b4d3dd6bdd82585a227bc78910
DiegoHR04 <huerta.rodriguez.diego.cbtis165@gmail.com>
2025-12-15 19:54:25 -0600

    Update sistema_caja.sql

--- base_datos/sistema_caja.sql
+++ base_datos/sistema_caja.sql
@@ -46,6 +46,11 @@ INSERT INTO usuario (nombre, apellido_paterno, apellido_materno, correo_instituc
 ('Administrador', 'General', 'Sistema', 'admin@itsx.edu.mx', NULL, NULL, NULL, '0000000000', '12345', NULL, 2),
 ('Juan', 'Bello', 'Zuñiga', '227O02930@itsx.edu.mx', 'bellozun12@gmail.com', 'BEZJ040831B99', 'BEZJ040831HVZLXNA8', '7841310586', 'contraseña1234', '4217470088983305', 1);
 
+-- Insertar SuperUsuario al crear la base de datos
+INSERT INTO usuario (nombre, apellido_paterno, apellido_materno, correo_institucional, correo_personal, rfc, curp, telefono, contrasena, tarjeta, id_rol) 
+VALUES 
+    ('Super', 'Usuario', 'Administrador', 'superadmin@itsx.edu.mx', 'super@gmail.com', NULL, NULL, '0000000000', 'super123', NULL, 3);
+
 CREATE TABLE IF NOT EXISTS ahorro (
     id_ahorro INT NOT NULL AUTO_INCREMENT,
     monto_ahorrado DECIMAL(10,2) NOT NULL DEFAULT 0.00,
