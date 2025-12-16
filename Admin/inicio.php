3c8e3d9117e3fa44bd95acd03e79394091a4ce15
TTmeXX <ahenandez98@gmail.com>
2025-12-15 20:43:23 -0600

    Update inicio.php for improved session management and pagination handling

--- Admin/inicio.php
+++ Admin/inicio.php
@@ -77,7 +77,7 @@ $pendientes = total_pendientes($pdo); // ['ahorro'=>x, 'prestamo'=>y, 'total'=>z
                 <li class="nav-item">
                     <a class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='ahorros.php') echo 'active'; ?>"
                         href="./gestion_prestamos.php">
-                        <i class="bi bi-cash-stack me-1"></i>Prestamos y Ahorros
+                        <i class="bi bi-cash-stack me-1"></i>Gestión de prestamos y ahorros
                     </a>
                 </li>
                 <li class="nav-item">
